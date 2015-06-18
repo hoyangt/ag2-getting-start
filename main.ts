de7d736a26d32bd5847d6bd95e0b59f193a5710a
Ho-Yang Tsai <hoyang.t@gmail.com>
2015-06-18 17:24:54 +0800

    Add readMe

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
 import {Component, View, bootstrap} from 'angular2/angular2';
 
 @Component({
-    selector: 'my-app'
+  selector: 'my-app'
 })
+
 @View({
-    template: '<h1>My first Angular 2 App</h1>'
+  template: '<h1>My first Angular 2 App</h1>'
 })
 
 class AppComponent {
